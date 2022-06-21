6abd75ca9d8edac0b8ffe8bb74ee98804ed77370
Meekun <orofinadedamola@gmail.com>
2022-06-21 17:29:44 +0100

    Added html and js files

---
+++ index.js
@@ -0,0 +1,6 @@
+let my_name = document.getElementById('name')
+let height = document.getElementById('height')
+let country = document.getElementById('country')
+my_name.innerText = 'Name: Orofin Adedamola Ayomikun'
+height.innerText = 'Height: 5ft 8inches'
+country.innerText = 'Country: Nigeria'
